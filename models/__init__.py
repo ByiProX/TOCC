1918a930939934e245f9752626fbddf08583c7ec
FRank5433 <frank5433@hotmail.com>
2018-02-02 22:06:10 +0800

    更新__init__

--- models/__init__.py
+++ models/__init__.py
@@ -3,5 +3,9 @@
 import android_db_models
 import user_bot_models
 import qun_friend_models
+import batch_sending_models
+import material_library_models
+import message_ext_models
+import production_consumption_models
 
 import_str = ""
