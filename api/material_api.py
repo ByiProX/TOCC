dc349c9c0219e78bb43aa4e8e449401c5fe6e2ce
frank5433 <zhangwenfa@doodod.com>
2018-02-24 12:52:56 +0800

    feat: 增加上传下载图片的逻辑

--- api/material_api.py
+++ api/material_api.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import logging
 from flask import request
 
 from configs.config import SUCCESS, ERR_PARAM_SET, main_api
 from core.user_core import UserLogin
 from utils.u_response import make_response
+
+logger = logging.getLogger('main')
 
 
 @main_api.route('/get_material_list', methods=['POST'])
@@ -22,3 +25,29 @@ def app_get_material_list():
         return make_response(ERR_PARAM_SET)
 
     raise NotImplementedError
+
+
+@main_api.route('/upload_file', methods=['POST'])
+def app_download_file_from_frontend():
+    """
+    从前端传过来的文件，存在cdn上
+    :return:
+    """
+    download_file = request.files['file']
+
+    # 确认图片是否在cdn上，如果在，则不进行上传，直接进行cdn与ml的绑定
+    # 如果不在，则先上传至cdn，然后将文件与ml绑定
+
+
+def _send_file_into_cdn():
+    """
+    将图片存入cdn
+    :return:
+    """
+
+
+def _get_file_from_cdn():
+    """
+    从cdn读取内容
+    :return:
+    """
