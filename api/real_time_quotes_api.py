e5f7d7082450315f244591a86acddbe4f3267dfc
FRank5433 <frank200800854@hotmail.com>
2018-02-27 22:49:34 +0800

    feat: 实时币价这边的接口

--- api/real_time_quotes_api.py
+++ api/real_time_quotes_api.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import logging
+
+from core.real_time_quotes_core import switch_func_real_time_quotes, get_rt_quotes_list_and_status, \
+    get_rt_quotes_preview
 from flask import request
 
 from configs.config import SUCCESS, ERR_PARAM_SET, main_api
@@ -20,7 +23,8 @@ def app_switch_func_real_time_quotes():
     if not (switch is True or switch is False):
         return make_response(ERR_PARAM_SET)
 
-    logger.error("ERR_NOT_IMPLEMENTED 功能未实现，先留出口")
+    status = switch_func_real_time_quotes(user_info, switch)
+    # logger.error("ERR_NOT_IMPLEMENTED 功能未实现，先留出口")
 
     if status == SUCCESS:
         return make_response(SUCCESS)
@@ -34,30 +38,31 @@ def app_get_rt_quotes_list_and_status():
     if status != SUCCESS:
         return make_response(status)
 
-    status = SUCCESS
-    logger.error("ERR_NOT_IMPLEMENTED 功能未实现，先留出口")
+    # status = SUCCESS
+    # logger.error("ERR_NOT_IMPLEMENTED 功能未实现，先留出口")
 
-    example = [
-        {
-            "coin_id": 1,
-            "coin_name": "比特币",
-            "logo": "http://"
-        },
-        {
-            "coin_id": 2,
-            "coin_name": "以太坊",
-            "logo": "http://"
-        },
-        {
-            "coin_id": 3,
-            "coin_name": "火币",
-            "logo": "http://"
-        },
-    ]
-    example_func = True
+    # example = [
+    #     {
+    #         "coin_id": 1,
+    #         "coin_name": "比特币",
+    #         "logo": "http://"
+    #     },
+    #     {
+    #         "coin_id": 2,
+    #         "coin_name": "以太坊",
+    #         "logo": "http://"
+    #     },
+    #     {
+    #         "coin_id": 3,
+    #         "coin_name": "火币",
+    #         "logo": "http://"
+    #     },
+    # ]
+    # example_func = True
+    status, res, func_status = get_rt_quotes_list_and_status(user_info)
 
     if status == SUCCESS:
-        return make_response(SUCCESS, coin_list=example, func_real_time_quotes=example_func)
+        return make_response(SUCCESS, coin_list=res, func_real_time_quotes=func_status)
     else:
         return make_response(status)
 
@@ -72,23 +77,24 @@ def app_get_rt_quotes_preview():
     if not coin_id:
         return make_response(ERR_PARAM_SET)
 
-    status = SUCCESS
-    logger.error("ERR_NOT_IMPLEMENTED 功能未实现，先留出口")
-
-    example = {
-        "coin_id": coin_id,
-        "name": 'BTC-比特币',
-        "logo": './assets/imgs/logo.png',
-        "keyword_list": ["BTC", "btc", "比特币"],
-        "price": '64634',
-        "all_price": '100921亿',
-        "rank": '第1名',
-        "coin_num": '3011万',
-        "add_rate": '+5.73%',
-        "change_num": '500万'
-    }
+    # status = SUCCESS
+    # logger.error("ERR_NOT_IMPLEMENTED 功能未实现，先留出口")
+    #
+    # example = {
+    #     "coin_id": coin_id,
+    #     "name": 'BTC-比特币',
+    #     "logo": './assets/imgs/logo.png',
+    #     "keyword_list": ["BTC", "btc", "比特币"],
+    #     "price": '64634',
+    #     "all_price": '100921亿',
+    #     "rank": '第1名',
+    #     "coin_num": '3011万',
+    #     "add_rate": '+5.73%',
+    #     "change_num": '500万'
+    # }
+    status, res = get_rt_quotes_preview(coin_id)
 
     if status == SUCCESS:
-        return make_response(SUCCESS, coin=example)
+        return make_response(SUCCESS, coin=res)
     else:
         return make_response(status)
