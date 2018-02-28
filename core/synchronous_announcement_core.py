ce3880cc02a1fbb24adda13308b739a2075054d3
frank5433 <zhangwenfa@doodod.com>
2018-02-28 23:04:09 +0800

    fix

--- core/synchronous_announcement_core.py
+++ core/synchronous_announcement_core.py
@@ -173,11 +173,11 @@ def match_which_user_should_get_notice(platform_name):
                     if len(wait_to_send_info.description) >= 105:
                         res_text = u"《" + wait_to_send_info.title + u"》\n来源：" + wait_to_send_info.from_source + \
                                    u"\n\n" + wait_to_send_info.description[:100] + u"...\n\n" + \
-                                   wait_to_send_info.origin_url + u"\n" + unicode(datetime.now())
+                                   wait_to_send_info.origin_url + u"\n" + unicode(datetime.now())[:19]
                     else:
                         res_text = u"《" + wait_to_send_info.title + u"》\n来源：" + wait_to_send_info.from_source + \
                                    u"\n\n" + wait_to_send_info.description + u"\n\n" + \
-                                   wait_to_send_info.origin_url + u"\n" + unicode(datetime.now())
+                                   wait_to_send_info.origin_url + u"\n" + unicode(datetime.now())[:19]
 
                     c_task.task_send_content = json.dumps({"text": res_text})
                     c_task.bot_username = bot_username
