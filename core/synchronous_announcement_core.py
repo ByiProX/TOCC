08f75fd75ff70dab8d5ad225b28f42be7b0ffe40
frank5433 <zhangwenfa@doodod.com>
2018-03-01 18:09:33 +0800

    fix: 在公告后面增加自己的标签

--- core/synchronous_announcement_core.py
+++ core/synchronous_announcement_core.py
@@ -174,12 +174,14 @@ def match_which_user_should_get_notice(platform_name):
                         res_text = u"《" + wait_to_send_info.title + u"》\n来源：" + wait_to_send_info.from_source + \
                                    u"\n\n" + wait_to_send_info.description[:100] + u"...\n\n" + \
                                    wait_to_send_info.origin_url + u"\n" + \
-                                   unicode(datetime.fromtimestamp(wait_to_send_info.timestamp / 1000))[:19]
+                                   unicode(datetime.fromtimestamp(wait_to_send_info.timestamp / 1000))[:19] + u"\n" + \
+                                   u"【友问币答】"
                     else:
                         res_text = u"《" + wait_to_send_info.title + u"》\n来源：" + wait_to_send_info.from_source + \
                                    u"\n\n" + wait_to_send_info.description + u"\n\n" + \
                                    wait_to_send_info.origin_url + u"\n" + \
-                                   unicode(datetime.fromtimestamp(wait_to_send_info.timestamp / 1000))[:19]
+                                   unicode(datetime.fromtimestamp(wait_to_send_info.timestamp / 1000))[:19] + u"\n" + \
+                                   u"【友问币答】"
                     c_task.task_send_content = json.dumps({"text": res_text})
                     c_task.bot_username = bot_username
                     c_task.message_received_time = now_time
