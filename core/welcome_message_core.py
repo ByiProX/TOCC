06f6845f297f58dced0cf6592b6c35de825fb565
frank5433 <zhangwenfa@doodod.com>
2018-03-01 14:55:50 +0800

    fix

--- core/welcome_message_core.py
+++ core/welcome_message_core.py
@@ -25,6 +25,28 @@ def _is_friend_into_qun():
     """
 
 
+first_into_a_qun_and_say_something = """
+大家好，我是你们的区块链学习小助手
+调用小助手规则：
+1.发送#币种名#
+助手回复该币种的价格，当前市值，排名，推荐交易所等
+2.不定时的推送各大交易所公告，让您掌握第一手消息
+3.发送#涨幅榜#
+助手回复24小时涨幅前20的名称，涨幅，价格
+4.发送#“币种”成交量榜#
+助手回复24小时成交量前20的名称，涨幅，24h成交额
+5.发送#交易所榜#
+助手推送Block.cc的交易量排行榜
+6.发送#新币发行#
+助手推送Block.cc的新币上线列表
+7.发送#行情分析#
+助手推送最新公众号文章
+8.发送#最新资讯#
+助手推送最新公众号文章
+9.发送#交易所公告#
+助手推送各个网站各个交易所公告"""
+
+
 def generate_welcome_message_c_task_into_new_qun(uqr_info, user_id, bot_username):
     c_task = ConsumptionTask()
 
@@ -35,7 +57,7 @@ def generate_welcome_message_c_task_into_new_qun(uqr_info, user_id, bot_username
     c_task.task_relevant_id = 0
 
     c_task.task_send_type = TASK_SEND_TYPE['text']
-    c_task.task_send_content = json.dumps({"text": "欢迎大家使用友问币答！~"})
+    c_task.task_send_content = json.dumps({"text": first_into_a_qun_and_say_something})
 
     c_task.bot_username = bot_username
 
