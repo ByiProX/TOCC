6d500809ec312497d51948563e0b435ea52db6af
zhaochang <zhaochang@doodod.com>
2018-04-14 00:33:09 +0800

    FIX: statis

--- api_v2/statis_chatroom_api.py
+++ api_v2/statis_chatroom_api.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import logging
-import time
-import datetime
+import time 
 import json
 
-from decimal import Decimal
-from flask import request
+from decimal import Decimal 
+from flask import request 
 
 from core_v2.user_core import UserLogin
 from models.android_db_models import AContact, AMember
@@ -14,131 +13,219 @@ from models.chatroom_member_models import ChatroomInfo, UserChatroomR, ChatroomO
     ChatroomActive
 from utils.u_model_json_str import verify_json
 from utils.u_response import make_response
-from configs.config import SUCCESS, main_api_v2, rds, db, DEFAULT_PAGE, DEFAULT_PAGE_SIZE, ERR_INVALID_PARAMS, \
-    ERR_WRONG_ITEM, \
+from configs.config import SUCCESS, main_api_v2,rds, db, DEFAULT_PAGE, DEFAULT_PAGE_SIZE, ERR_INVALID_PARAMS, ERR_WRONG_ITEM, \
     SCOPE_WEEK, SCOPE_ALL, USER_CHATROOM_R_PERMISSION_1
-from utils.u_time import get_time_window_by_scope, datetime_to_timestamp_utc_8
-
+ 
 from models_v2.base_model import *
 
+import datetime
 logger = logging.getLogger('main')
+
+def modelList2Arr(mlist):
+    ret = []
+    if mlist :
+        for li in mlist:
+            ret.append(li.to_json())
+    return ret
+
+
+
+def getGrouplist(client_id):
+    ret = {}
+    if client_id :
+        groupList = BaseModel.fetch_all('client_group_r', ['group_name','group_id','status'] ,BaseModel.where("=", "client_id", client_id))
+        return modelList2Arr(groupList);
+
+
+def getClientQunWithGroup(client_id,group_id,page=1,pagesize=30,order=''):
+    ret = {}
+    if group_id :
+        _where = ["and",["=","client_id",client_id],["=","group_id",group_id]]
+        _where = BaseModel.where_dict(_where)
+        qunList = BaseModel.fetch_all('client_qun_r', ['chatroomname','group_id','group_info','status'],_where,page=1,pagesize=pagesize,orderBy=order)
+        return modelList2Arr(qunList);
+
+
+@main_api_v2.route('/statistics_chatroomone', methods = ['POST'])
+def statistics_chatroomone():
+    status, user_info = UserLogin.verify_token(request.json.get('token'))
+    if status != SUCCESS:
+        return make_response(status)
+    chatroomname = request.json.get('chatroomname')
+    if not chatroomname:
+        return make_response(ERR_INVALID_PARAMS)
+    data =  BaseModel.fetch_one('a_chatroom', '*' ,BaseModel.where("=", "chatroomname", chatroomname))
+    data = data.to_json()
+    return make_response(SUCCESS,chatroominfo=data)
+
+
+#根据group id 获取群list
+@main_api_v2.route('/statistics_chatroom_qun', methods = ['POST'])
+def chatroom_statistics_chatroom_qun():
+    group_id = request.json.get('group_id')
+    if not group_id:
+        return make_response(ERR_INVALID_PARAMS)
+
+    status, user_info = UserLogin.verify_token(request.json.get('token'))
+    if status != SUCCESS:
+        return make_response(status)
+
+    page = request.json.get('page',1)
+    order = request.json.get('order','')
+    pagesize = request.json.get('pagesize', 30) 
+    qunList = getClientQunWithGroup(user_info.client_id,group_id,page,pagesize,order)
+    return make_response(SUCCESS,chatroom_group_qun = qunList) 
+
+
+#获取 group list
+@main_api_v2.route('/statistics_chatroom_group', methods = ['POST'])
+def chatroom_statistics_chatroom_group():
+    status, user_info = UserLogin.verify_token(request.json.get('token'))
+    if status != SUCCESS:
+        return make_response(status)
+    groupList = getGrouplist(user_info.client_id)
+    return make_response(SUCCESS,chatroom_group = groupList) 
+ 
 
 
 @main_api_v2.route('/statistics_chatroom', methods = ['POST'])
 def chatroom_statistics_chatroom():
-    # verify_json()
+    #verify_json()
     status, user_info = UserLogin.verify_token(request.json.get('token'))
     if status != SUCCESS:
         return make_response(status)
-    # date_type=1 今日实时，每10分钟计算一次，cache 10分钟
-    # date_type=2 昨日，从daily取，cache10分钟
-    # date_type=3 7日，从daily计算过去7天，cache 10分钟
-    # date_type=4 全部，从total表取，cache10分钟
+    
+
+        
+    #group_id 群分组ID ， cache key =  dateType_clientId_groupId,
+    #cache value= {time():data}  cache 10分钟
+
+    group_id = request.json.get('group_id', '')
+    chatroomname_list = []
+    if group_id:
+        cache_key = 'c_'+group_id
+        #这里需要根据groupId 取 qunID list，或者前端post过来 qunID list
+        qunList = getClientQunWithGroup(user_info.client_id,group_id)
+        for qli in qunList:
+            chatroomname_list.append(qli['chatroomname'])
+
+    #date_type=1 今日实时，每10分钟计算一次，cache 10分钟
+    #date_type=2 昨日，从daily取，cache10分钟
+    #date_type3=7日，从daily计算过去7天，cache 10分钟
+    #date_type4=30日，从daily计算过去30天，cache 10分钟
+    #date_type=5 全部，从total表取，cache10分钟
     #   cache key=  dateType_clientId, cache value= {time():data}
     date_type = request.json.get('date_type', 1)
     run_hour = ''
     table = 'statistics_chatroom_hour'
     _timestamp = int(time.time())
     _where = []
-    if (date_type == 1):
-        run_hour = int(time.strftime('%H', time.localtime(time.time())))
-        if (run_hour == 0):
-            return make_response(SUCCESS)
-        _where = ["and", ["<=", "run_hour", run_hour], ["=", "client_id", user_info.client_id]]
-    elif (date_type == 2):
-        day_diff = str(datetime.date.today() - datetime.timedelta(days = 1))
-        time_arr = time.strptime(str(day_diff), "%Y-%m-%d")
-        timestamp_diff = int(time.mktime(time_arr))
-        _where = {"date": timestamp_diff, "client_id": user_info.client_id}
-        # where = BaseModel.where_dict({"date":timestamp_diff,"client_id":user_info.client_id})
+    if(date_type==1):
+        run_hour = int( time.strftime('%H',time.localtime(time.time())))
+        last_update_time = _timestamp - 600
+        if(run_hour==0):
+            return make_response(SUCCESS,chatroom_list = [], last_update_time = last_update_time) 
+        _where = ["and",["<=","run_hour",run_hour],["=","client_id",user_info.client_id],["in","chatroomname",chatroomname_list]] 
+    elif(date_type==2):
+        timestamp_diff = getTimeStamp(1)
+        last_update_time =  timestamp_diff + 600 
+       # _where ={"date":timestamp_diff,"client_id":user_info.client_id}
+        _where = ["and",["=","date",timestamp_diff],["=","client_id",user_info.client_id],["in","chatroomname",chatroomname_list]]  
         table = 'statistics_chatroom_daily'
-    elif (date_type == 3):
-        day_diff = str(datetime.date.today() - datetime.timedelta(days = 7))
-        time_arr = time.strptime(str(day_diff), "%Y-%m-%d")
-        timestamp_diff = int(time.mktime(time_arr))
+    elif(date_type==3):
+        timestamp_diff = getTimeStamp(7)
+        last_update_time =  getTimeStamp(1) + 600 
         table = 'statistics_chatroom_daily'
-
-        _where = ["and", [">=", "date", timestamp_diff], ["=", "client_id", user_info.client_id]]
-    elif (date_type == 4):
-        table = 'statistics_chatroom_total'
-        # where = BaseModel.where("=", "client_id", user_info.client_id)
-        _where = {"client_id": user_info.client_id}
-
-    cache_key = '_st' + str(date_type) + '_' + str(user_info.client_id)
-
-    # group_id 群分组ID ， cache key =  dateType_clientId_groupId,
-    # cache value= {time():data}  cache 10分钟
-    group_id = request.json.get('group_id', '')
-    if group_id:
-        # 这里需要根据groupId 取 qunID list，或者前端post过来 qunID list
-        group_id = ''
-
-    # 群ID，cache key = dateType_clientId_chatroomname ,cache 10分钟
+        _where = ["and",[">=","date",timestamp_diff],["=","client_id",user_info.client_id],["in","chatroomname",chatroomname_list]] 
+    elif(date_type==4):
+        timestamp_diff = getTimeStamp(30)
+        last_update_time =  getTimeStamp(1) + 600 
+        table = 'statistics_chatroom_daily'
+        _where = ["and",[">=","date",timestamp_diff],["=","client_id",user_info.client_id],["in","chatroomname",chatroomname_list]] 
+    elif(date_type==5):
+        last_update_time =  getTimeStamp(1) + 1200 
+        table = 'statistics_chatroom_total' 
+        #_where ={"client_id":user_info.client_id}
+        _where = ["and",["=","client_id",user_info.client_id],["in","chatroomname",chatroomname_list]] 
+    
+    cache_key = '_st'+str(date_type)+'_'+str(user_info.client_id)
+ 
+    #群ID，cache key = dateType_clientId_chatroomname ,cache 10分钟
     chatroomname = request.json.get('chatroomname')
-    if chatroomname:
-        _where = {"chatroomname": chatroomname, "client_id": user_info.client_id}
-        cache_key = cache_key + '_' + chatroomname
+    if chatroomname:  
+        _where ={"chatroomname":chatroomname,"client_id":user_info.client_id}
+        cache_key = cache_key+'_'+chatroomname
+        
 
-    page = request.json.get('page', 1)
+    
+    
+    page = request.json.get('page',1)
     pagesize = request.json.get('pagesize', 30)
-    cache_key = cache_key + '_p' + str(page)
+    cache_key = cache_key+'_p'+str(page)
 
-    # 规则是 active_count 是字段名，去掉count，变成 active_asc or active_desc
+    #规则是 active_count 是字段名，去掉count，变成 active_asc or active_desc
     order = request.json.get('order')
     if order:
-        cache_key = cache_key + '_' + order
+        cache_key = cache_key+'_'+order
         order = order.split('_')
-        order = order[0] + '_count' + ' ' + order[1]
-
+        order = order[0]+'_count'+' '+order[1]
+ 
+    
     cacheData = rds.get(cache_key)
-    # cacheData = 0
     if cacheData:
-        print "cache hit"
-        cacheData = json.loads(cacheData)
+        print "cache hit" 
+        cacheData =  json.loads(cacheData)
         for cd in cacheData:
             return make_response(SUCCESS, chatroom_list = cacheData[cd], last_update_time = cd)
+         
 
     _where = BaseModel.where_dict(_where)
-
-    print "------_where:::-----------\n", _where, order, "-----------------\n"
-
-    total_count = BaseModel.count(table, _where)
-    chatroom_statis = BaseModel.fetch_all(table, '*', _where, page = 1, pagesize = pagesize, orderBy = order)
+    
+    print "------_where:::-----------\n", _where,order ,"-----------------\n"
+    
+    chatroom_statis = BaseModel.fetch_all(table, '*' ,_where,page=1,pagesize=pagesize,orderBy=order)
     chatroom_json_list = []
+    if(len(chatroom_statis)<1):
+        return make_response(SUCCESS, chatroom_list = [], last_update_time = last_update_time)
 
+    
+    
     chatroomnames = []
     for st in chatroom_statis:
         _stjson = st.to_json()
+        print _stjson
         chatroomnames.append(_stjson['chatroomname'])
         chatroom_json_list.append(_stjson)
-
+    
     qunInfo = getQunInfo(chatroomnames)
-    print "==========\n", qunInfo, "============\n"
+    print "==========\n",qunInfo,"============\n"
+    
 
-    if (len(qunInfo) > 0):
+    if (len(qunInfo)>0) :
         for ctlist in chatroom_json_list:
             if qunInfo.has_key(ctlist['chatroomname']):
                 ctlist.update(qunInfo[ctlist['chatroomname']])
-                print '9999999999', ctlist, '00000000000'
+                print '9999999999',ctlist,'00000000000'
+     
+    if(len(chatroom_json_list)>0):
+        rds.set(cache_key,json.dumps({last_update_time:chatroom_json_list}))
+        rds.expire(cache_key,10)
 
-    last_update_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(_timestamp))
+    return make_response(SUCCESS, chatroom_list = chatroom_json_list, last_update_time = last_update_time)
 
-    if (len(chatroom_json_list) > 0):
-        rds.set(cache_key, json.dumps({last_update_time: chatroom_json_list}))
-        rds.expire(cache_key, 60 * 10)
-
-    return make_response(SUCCESS, chatroom_list = chatroom_json_list, last_update_time = last_update_time, total_count = total_count)
-
-
-# chatroomnames must array
+#chatroomnames must array
 def getQunInfo(chatroomnames):
     ret = {}
-    if chatroomnames:
-        qunInfo = BaseModel.fetch_all('a_chatroom', ['chatroomname', 'nickname', 'avatar_url'],
-                                      BaseModel.where("in", "chatroomname", chatroomnames))
-        if (qunInfo):
+    if chatroomnames :
+        qunInfo = BaseModel.fetch_all('a_chatroom', ['chatroomname','nickname','avatar_url'] ,BaseModel.where("in", "chatroomname", chatroomnames))
+        if(qunInfo):
             for qf in qunInfo:
                 _qfjson = qf.to_json()
-                print '6666666666', _qfjson
-                ret[_qfjson["chatroomname"]] = _qfjson
-    return ret
+                print '6666666666',_qfjson
+                ret[_qfjson["chatroomname"]]= _qfjson 
+    return ret            
+
+def getTimeStamp(d_diff=0):
+    day_diff  = str(datetime.date.today() - datetime.timedelta(days=d_diff))
+    time_arr = time.strptime(str(day_diff), "%Y-%m-%d")
+    return int(time.mktime(time_arr))
--- api_v2/statis_member_api.py
+++ api_v2/statis_member_api.py
@@ -2,10 +2,9 @@
 
 import logging
 import threading
-import time
-import datetime
+import time 
 from copy import copy
-
+ 
 from flask import request
 from sqlalchemy import func
 
@@ -22,127 +21,231 @@ from utils.u_time import datetime_to_timestamp_utc_8
 
 from models_v2.base_model import *
 
-logger = logging.getLogger('main')
+import datetime 
+from urllib import urlencode,quote
 
+logger = logging.getLogger('main')
+ 
+
+def modelList2Arr(mlist):
+    ret = []
+    if mlist :
+        for li in mlist:
+            ret.append(li.to_json())
+    return ret
+
+  
+#根据昵称搜索微信用户
+def member_search(nickname): 
+    print "\n\n\n ----  member_search ----------\n"
+    _where = ["like","nickname", nickname.encode("utf-8")]
+    _where = BaseModel.where_dict(_where)
+    data = BaseModel.fetch_all('a_contact', '*' ,_where)
+    data = modelList2Arr(data)
+    print '==== ',_where,'== =======',data,'=== ===='
+    print "\n ----  member_search end  ----------\n\n\n"
+    return  data
+
+
+#获取一个群里所有成员wxID
+def getMembersOfQun(chatroomname): 
+    print "\n\n\n ----  getMembersOfQun ----------\n"
+    _where = ["=","chatroomname", chatroomname]
+    _where = BaseModel.where_dict(_where)
+    data = BaseModel.fetch_all('a_member', '*' ,_where)
+    data = modelList2Arr(data)
+    print '===========',_where,'===============',data,'=========='
+    print "\n ----  getMembersOfQun  end  ----------\n\n\n"
+    return  data
+  
+ 
 
 @main_api_v2.route('/statistics_member', methods = ['POST'])
 def statistics_member():
-    # verify_json()
+    #verify_json()
     status, user_info = UserLogin.verify_token(request.json.get('token'))
     if status != SUCCESS:
         return make_response(status)
-    # date_type=1 今日实时，每10分钟计算一次，cache 10分钟
-    # date_type=2 昨日，从daily取，cache10分钟
-    # date_type3=7日，从daily计算过去7天，cache 10分钟
-    # date_type=4 全部，从total表取，cache10分钟
+    #date_type=1 今日实时，每10分钟计算一次，cache 10分钟
+    #date_type=2 昨日，从daily取，cache10分钟  每晚上 0:10
+    #date_type3=7日，从daily计算过去7天，cache 10分钟
+    #date_type4=30日，从daily计算过去30天，cache 10分钟
+    #date_type=5 全部，从total表取，cache10分钟
     #   cache key=  dateType_clientId, cache value= {time():data}
     date_type = request.json.get('date_type', 1)
     run_hour = ''
     table = 'statistics_member_hour'
     _timestamp = int(time.time())
+    _dailyRunStamp = 0
     _where = []
+    useranems = []
     cache_key = ''
-
-    chatroomname = request.json.get('chatroomname')
+    
+    chatroomname = request.json.get('chatroomname' )
     if not chatroomname:
         return make_response(ERR_INVALID_PARAMS)
     else:
-        # 群ID，cache key = chatroomname ,cache 10分钟
-        _where = {"chatroomname": chatroomname}
-        cache_key = 'st_' + chatroomname
+        #群ID，cache key = chatroomname ,cache 10分钟 
+        _where ={"chatroomname":chatroomname}
+        cache_key =  'st_'+chatroomname
+    
+    
+    search_users = []
+    nickname = request.json.get('keywords') 
+    if nickname:
+        print "\n ------ params:",nickname,chatroomname,"------\n"
+        users_search = member_search(nickname)
+        if users_search: 
+            membersOfQun = getMembersOfQun(chatroomname)
+            if membersOfQun:
+                qmembers = json.loads(membersOfQun[0]['members'])
+                for u in users_search:
+                    for qm in qmembers:
+                        if(u['username'] == qm['username']):
+                            search_users.append(u)
+                            useranems.append(u['username'])
+    
+    
+    username = request.json.get('username')
+    if username:
+        useranems.append(username)
 
-    if (date_type == 1):
-        run_hour = int(time.strftime('%H', time.localtime(time.time())))
-        if (run_hour == 0):
-            return make_response(SUCCESS)
-        _where = ["and", ["<=", "run_hour", run_hour], ["=", "chatroomname", chatroomname]]
-    elif (date_type == 2):
-        day_diff = str(datetime.date.today() - datetime.timedelta(days = 1))
-        time_arr = time.strptime(str(day_diff), "%Y-%m-%d")
-        timestamp_diff = int(time.mktime(time_arr))
-        _where = {"date": timestamp_diff, "chatroomname": chatroomname}
-        # where = BaseModel.where_dict({"date":timestamp_diff,"chatroomname":chatroomname})
-        table = 'statistics_member_daily'
-    elif (date_type == 3):
-        day_diff = str(datetime.date.today() - datetime.timedelta(days = 7))
-        time_arr = time.strptime(str(day_diff), "%Y-%m-%d")
-        timestamp_diff = int(time.mktime(time_arr))
-        table = 'statistics_member_daily'
+    print "\n ------ search_users :",search_users,"------\n"
+    print "\n ------ useranems :",useranems,"------\n"
 
-        _where = ["and", [">=", "date", timestamp_diff], ["=", "chatroomname", chatroomname]]
-    elif (date_type == 4):
+    if(date_type==1):
+        run_hour = int( time.strftime('%H',time.localtime(time.time())))
+        last_update_time = _timestamp - 600
+        if(run_hour==0):
+            return make_response(SUCCESS,member_list = [], last_update_time = last_update_time) 
+        _where = ["and",["<=","run_hour",run_hour],["=","chatroomname",chatroomname],["in","username",useranems]] 
+    elif(date_type==2):
+        timestamp_diff = getTimeStamp(1)
+        last_update_time =  timestamp_diff + 600  
+        _where = ["and",["=","date",timestamp_diff],["=","chatroomname",chatroomname],["in","username",useranems]] 
+        #where = BaseModel.where_dict({"date":timestamp_diff,"chatroomname":chatroomname})
+        table = 'statistics_member_daily' 
+    elif(date_type==3):
+        timestamp_diff = getTimeStamp(7)
+        last_update_time =  getTimeStamp(1) + 600 
+        table = 'statistics_member_daily' 
+        _where = ["and",[">=","date",timestamp_diff],["=","chatroomname",chatroomname],["in","username",useranems]] 
+    elif(date_type==4):
+        timestamp_diff = getTimeStamp(30)
+        last_update_time =  getTimeStamp(1) + 600 
+        table = 'statistics_member_daily' 
+        _where = ["and",[">=","date",timestamp_diff],["=","chatroomname",chatroomname],["in","username",useranems]]  
+    elif(date_type==5):
         table = 'statistics_member_total'
-        # where = BaseModel.where("=", "chatroomname", chatroomname)
-        _where = {"chatroomname": chatroomname}
+        last_update_time =  getTimeStamp(1) + 1200 
+        #where = BaseModel.where("=", "chatroomname", chatroomname)
+       # _where ={"chatroomname":chatroomname}
+        _where = ["and",["=","chatroomname",chatroomname],["in","username",useranems]] 
+    
+    cache_key = cache_key +'_'+ str(date_type) 
+ 
 
-    cache_key = cache_key + '_' + str(date_type)
-
-    page = request.json.get('page', 1)
+    page = request.json.get('page',1)
     pagesize = request.json.get('pagesize', 30)
-    cache_key = cache_key + '_p' + str(page)
+    cache_key = cache_key+'_p'+str(page)
 
-    # 规则是 active_count 是字段名，去掉count，变成 active_asc or active_desc
+    #规则是 active_count 是字段名，去掉count，变成 active_asc or active_desc
     order = request.json.get('order')
     if order:
-        cache_key = cache_key + '_' + order
+        cache_key = cache_key+'_'+order
         order = order.split('_')
-        order = order[0] + '_count' + ' ' + order[1]
-
+        order = order[0]+'_count'+' '+order[1]
+    
+    
     cacheData = rds.get(cache_key)
-    # cacheData = 0
+    cacheData = 0
     if cacheData:
-        print "cache hit"
-        cacheData = json.loads(cacheData)
-        for cd in cacheData:
-            return make_response(SUCCESS, chatroom_list = cacheData[cd], last_update_time = cd)
-
+        print "cache hit" 
+        cacheData =  json.loads(cacheData)
+        for cd in cacheData: 
+            return make_response(SUCCESS, member_list = cacheData[cd], last_update_time = cd)
+         
+    
+    print "\n ------_where value = -----------\n", _where,order ,"-----------------\n"
     _where = BaseModel.where_dict(_where)
-
-    print "------_where:::-----------\n", _where, order, "-----------------\n"
-
-    member_statis = BaseModel.fetch_all(table, '*', _where, page = 1, pagesize = pagesize, orderBy = order)
+    
+    member_statis = BaseModel.fetch_all(table, '*' ,_where,page=1,pagesize=pagesize,orderBy=order)
     member_json_list = []
+    
+    print "\n ------member_statis = -----------\n", modelList2Arr(member_statis) ,"-----------------\n"
 
-    if (len(member_statis) <= 0):
-        return make_response(SUCCESS)
-
+    if(len(member_statis)<=0):
+        return make_response(SUCCESS,member_list=[],last_update_time=last_update_time)
+ 
     member_client_id = member_statis[0].client_id
-    # memberfirst['client_id'] = 2
-    if (member_client_id != user_info.client_id):
+   # memberfirst['client_id'] = 2
+    if(member_client_id != user_info.client_id):
         return make_response(ERR_WRONG_ITEM)
-
-    wxIds = []
+    
+    if len(useranems) > 0:
+        wxIds = useranems
+        userInfo = search_users
+    else:
+        wxIds = []
+       
     for st in member_statis:
         _stjson = st.to_json()
         wxIds.append(_stjson['username'])
         member_json_list.append(_stjson)
-
+    
     userInfo = getUserInfo(wxIds)
-    print "==========\n", userInfo, "============\n"
+     
+    print "\n========== userInfo = \n",userInfo,"============\n"
+    
 
-    if (len(userInfo) > 0):
+    if (len(userInfo)>0) :
         for ctlist in member_json_list:
             if userInfo.has_key(ctlist['username']):
                 ctlist.update(userInfo[ctlist['username']])
-                print '9999999999', ctlist, '00000000000'
+                print '9999999999',ctlist,'00000000000'
+    
+    #last_update_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(_timestamp))
+    #last_update_time = _timestamp
+    
+    print "\n========== member_json_list = \n",member_json_list,"============\n"
 
-    last_update_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(_timestamp))
-    if (len(member_json_list) > 0):
-        rds.set(cache_key, json.dumps({last_update_time: member_json_list}))
-        rds.expire(cache_key, 60 * 10)
+    if(len(member_json_list)>0):
+        rds.set(cache_key,json.dumps({last_update_time:member_json_list}))
+        rds.expire(cache_key,10)
 
     return make_response(SUCCESS, member_list = member_json_list, last_update_time = last_update_time)
 
 
-# wxIds must array
+
+
+
+@main_api_v2.route('/statistics_memberone', methods = ['POST'])
+def statistics_memberon():
+    status, user_info = UserLogin.verify_token(request.json.get('token'))
+    if status != SUCCESS:
+        return make_response(status)
+    username = request.json.get('username')
+    if not username:
+        return make_response(ERR_INVALID_PARAMS)
+    userinfo =  BaseModel.fetch_one('a_contact', '*' ,BaseModel.where("=", "username", username))
+    userinfo = userinfo.to_json()
+    return make_response(SUCCESS,userinfo=userinfo)
+
+
+#wxIds must array
 def getUserInfo(wxIds):
     ret = {}
-    if wxIds:
-        userInfo = BaseModel.fetch_all('a_contact', ['nickname', 'username', 'avatar_url'],
-                                       BaseModel.where("in", "username", wxIds))
-        if (userInfo):
+    if wxIds : 
+        userInfo = BaseModel.fetch_all('a_contact', ['nickname','username','avatar_url'] ,BaseModel.where("in", "username", wxIds))
+        if(userInfo):
             for qf in userInfo:
                 _qfjson = qf.to_json()
-                print '6666666666', _qfjson
-                ret[_qfjson["username"]] = _qfjson
+                print '6666666666',_qfjson
+                ret[_qfjson["username"]]= _qfjson 
     return ret
+    
+
+def getTimeStamp(d_diff=0):
+    day_diff  = str(datetime.date.today() - datetime.timedelta(days=d_diff))
+    time_arr = time.strptime(str(day_diff), "%Y-%m-%d")
+    return int(time.mktime(time_arr))
