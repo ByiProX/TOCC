f53d9af983bf662f12fb92af3d00c6d4d9602819
ZYunH <workvl@163.com>
2018-04-14 19:06:00 +0800

    fix

--- api_v2/events_api.py
+++ api_v2/events_api.py
@@ -158,7 +158,7 @@ def create_event():
         }
     }
     try:
-        create_chatroom_resp = requests.post('http://192.168.1.10/android/send_message', json=create_chatroom_dict)
+        create_chatroom_resp = requests.post('http://192.168.1.10:5000/android/send_message', json=create_chatroom_dict)
         print(create_chatroom_resp.text)
     except Exception as e:
         logger.warning('Create chatroom request error:{}'.format(e))
