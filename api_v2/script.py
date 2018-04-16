ff4fefe74fa58de23f7aff53d3a5aa6b81c172b5
zhaochang <zhaochang@doodod.com>
2018-04-16 21:04:13 +0800

    FIX

---
+++ api_v2/script.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+import time
+from flask import request
+
+from configs.config import main_api_v2
+from models_v2.base_model import BaseModel
+
+logger = logging.getLogger('main')
+
+
+@main_api_v2.route("/script", methods = ['POST'])
+def script():
+    uqr = BaseModel.fetch_by_id("client_qun_r", "5ad46153f5d7e26589658ba7")
+    uqr.group_id = u"4_0"
+    uqr.update()
