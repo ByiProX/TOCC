36da08bbf38bb7b5fd206fa924345525f7926e9d
zhaochang <zhaochang@doodod.com>
2018-04-16 22:13:43 +0800

    FIX

--- core_v2/send_msg.py
+++ core_v2/send_msg.py
@@ -19,13 +19,13 @@ def send_msg_to_android(bot_username, message_list, to_list):
     url = ANDROID_SERVER_URL_SEND_MASS_MESSAGE
     logger.info(u"安卓发送任务. url: %s." % url)
     response = requests.post(url = url, json = data)
-    response_json = json.loads(response.content)
-    err_code = response_json.get("err_code")
-    if err_code == 0:
-        return SUCCESS
-    else:
-        logger.info(u"任务发送失败. bot_username: %s." % bot_username)
-        return ERR_UNKNOWN_ERROR
+    if response.request.status_code == 200:
+        response_json = json.loads(response.content)
+        err_code = response_json.get("err_code")
+        if err_code == 0:
+            return SUCCESS
+    logger.info(u"任务发送失败. bot_username: %s." % bot_username)
+    return ERR_UNKNOWN_ERROR
 
 
 def send_ws_to_android(bot_username, data):
@@ -34,10 +34,10 @@ def send_ws_to_android(bot_username, data):
     logger.info(u"send_ws_to_android. url: %s." % url)
     response = requests.post(url = url, json = {"bot_username": bot_username,
                                                 "data": data})
-    response_json = json.loads(response.content)
-    err_code = response_json.get("err_code")
-    if err_code == 0:
-        return SUCCESS
-    else:
-        logger.info(u"send_ws_to_android failed. bot_username: %s." % bot_username)
-        return ERR_UNKNOWN_ERROR
+    if response.request.status_code == 200:
+        response_json = json.loads(response.content)
+        err_code = response_json.get("err_code")
+        if err_code == 0:
+            return SUCCESS
+    logger.info(u"send_ws_to_android failed. bot_username: %s." % bot_username)
+    return ERR_UNKNOWN_ERROR
