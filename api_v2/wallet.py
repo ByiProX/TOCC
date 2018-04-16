aef4e7143d57c99d2a6c688cc9c05000a22a4187
zhaochang <zhaochang@doodod.com>
2018-04-16 20:30:13 +0800

    FIX

--- api_v2/wallet.py
+++ api_v2/wallet.py
@@ -86,7 +86,7 @@ def app_wallets():
     data = []
     wxIds = []
     for w in wallet_list:
-        _w = w.to_json()
+        _w = w.to_json_full()
         wxIds.append(_w['username'])
 
         # wxUserInfo = BaseModel.fetch_one('a_contact', 'nickname,avatar_url' ,BaseModel.where_dict({"username":_w['username']}))
