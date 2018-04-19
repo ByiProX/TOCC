d88e0acb0d6dbe80fd298bc4ec709c4c806316b2
scofield <75251773@qq.com>
2018-04-19 21:49:42 +0800

    debug redis msg

--- core_v2/message_core.py
+++ core_v2/message_core.py
@@ -109,7 +109,7 @@ def count_msg(msg):
         content = str_to_unicode(msg.content)
         chatroomname = msg.talker
         username = msg.real_talker
-        msg_type = msg.type
+        msg_type = msg.type 
         logger.info(u"msg id is : %s. " % msg.to_json())
         logger.info(u"msg content is : %s. " % msg.msg_local_id)
 
@@ -133,8 +133,7 @@ def count_msg(msg):
 
             return
         chat_logs_type = CHAT_LOGS_TYPE_2
-        #rds_lpush(chat_logs_type, msg.get_id())
-        rds_lpush(chat_logs_type, msg.msg_local_id)
+        rds_lpush(chat_logs_type, msg.get_id())
 
             # 被邀请入群
             # Content="frank5433"邀请你和"秦思语-Doodod、磊"加入了群聊
