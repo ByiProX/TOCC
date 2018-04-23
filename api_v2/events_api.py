8e8bacf5213d43b306c9421d262c6525fc3c5c48
ZYunH <workvl@163.com>
2018-04-23 22:00:03 +0800

    event_api fix

--- api_v2/events_api.py
+++ api_v2/events_api.py
@@ -48,8 +48,7 @@ def create_event_init():
             if i.is_finish == 0:
                 previous_event = BaseModel.fetch_one('events', '*',
                                                      BaseModel.where_dict({"owner": owner, "is_finish": 0}))
-                alive_qrcode_url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxc3bc48b4c40651fd&redirect_uri=http%3a%2f%2ftest2.xuanren360.com%2fchatroom.html&response_type=code&scope=snsapi_userinfo&state={}#wechat_redirect'.format(
-                    previous_event.events_id)
+                alive_qrcode_url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxc3bc48b4c40651fd&redirect_uri=http%3a%2f%2ftest2.xuanren360.com%2fchatroom.html&response_type=code&scope=snsapi_userinfo&state={}#wechat_redirect'.format(previous_event.events_id)
                 event_id = previous_event.events_id
                 return response({'err_code': 0, 'content': {'alive_qrcode_url': alive_qrcode_url,
                                                             'fission_word_1': '嗨！恭喜您即将获取「3点钟无眠区块链」听课资格！ 1.转发以上图片+文字到朋友圈或者100以上群聊中 2.不要屏蔽好友，转发后截图发至本群 3.转发后在本群等待听课即可 分享图片及内容如下 ↓↓↓↓↓ ',
@@ -68,7 +67,7 @@ def create_event_init():
                                          BaseModel.where_dict({"owner": owner, "is_finish": 0}))
     alive_qrcode_url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxc3bc48b4c40651fd&redirect_uri=http%3a%2f%2ftest2.xuanren360.com%2fchatroom.html&response_type=code&scope=snsapi_userinfo&state={}#wechat_redirect'.format(previous_event.events_id)
     previous_event.alive_qrcode_url = alive_qrcode_url
-    previous_event.update()
+    previous_event.save()
     # Static word.
     result = {
         'fission_word_1': '嗨！恭喜您即将获取「3点钟无眠区块链」听课资格！ 1.转发以上图片+文字到朋友圈或者100以上群聊中 2.不要屏蔽好友，转发后截图发至本群 3.转发后在本群等待听课即可 分享图片及内容如下 ↓↓↓↓↓ ',
