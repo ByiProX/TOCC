19c6942499d6f2123cee9077ec5775da12534f76
zhaochang <zhaochang@doodod.com>
2018-04-25 19:13:58 +0800

    FIX

--- api_v2/statis_chatroom_api.py
+++ api_v2/statis_chatroom_api.py
@@ -177,7 +177,7 @@ def chatroom_statistics_chatroom():
         table = 'statistics_chatroom_daily'
         _where = ["and", [">=", "date", timestamp_diff], ["=", "client_id", user_info.client_id],
                   ["in", "chatroomname", chatroomname_list]]
-        group_by = json.dumps({"_id": "&chatroomname", "speak_count": {"$sum": {"$multiply": ["$speak_count"]}},
+        group_by = json.dumps({"_id": "$chatroomname", "speak_count": {"$sum": {"$multiply": ["$speak_count"]}},
                     "in_count": {"$sum": {"$multiply": ["$in_count"]}},
                     "out_count": {"$sum": {"$multiply": ["$out_count"]}},
                     "memberchange_count": {"$sum": {"$multiply": ["$memberchange_count"]}},
@@ -187,7 +187,7 @@ def chatroom_statistics_chatroom():
         table = 'statistics_chatroom_daily'
         _where = ["and", [">=", "date", timestamp_diff], ["=", "client_id", user_info.client_id],
                   ["in", "chatroomname", chatroomname_list]]
-        group_by = json.dumps({"_id": "&chatroomname", "speak_count": {"$sum": {"$multiply": ["$speak_count"]}},
+        group_by = json.dumps({"_id": "$chatroomname", "speak_count": {"$sum": {"$multiply": ["$speak_count"]}},
                     "in_count": {"$sum": {"$multiply": ["$in_count"]}},
                     "out_count": {"$sum": {"$multiply": ["$out_count"]}},
                     "memberchange_count": {"$sum": {"$multiply": ["$memberchange_count"]}},
@@ -197,7 +197,7 @@ def chatroom_statistics_chatroom():
         table = 'statistics_chatroom_total'
         # _where ={"client_id":user_info.client_id}
         _where = ["and", ["=", "client_id", user_info.client_id], ["in", "chatroomname", chatroomname_list]]
-        group_by = json.dumps({"_id": "&chatroomname", "speak_count": {"$sum": {"$multiply": ["$speak_count"]}},
+        group_by = json.dumps({"_id": "$chatroomname", "speak_count": {"$sum": {"$multiply": ["$speak_count"]}},
                     "in_count": {"$sum": {"$multiply": ["$in_count"]}},
                     "out_count": {"$sum": {"$multiply": ["$out_count"]}},
                     "memberchange_count": {"$sum": {"$multiply": ["$memberchange_count"]}},
