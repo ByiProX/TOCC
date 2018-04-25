0682b0560acbd303cea62cc2f8fb04d001bf17b5
ZYunH <workvl@163.com>
2018-04-25 14:25:34 +0800

    sensitive improve

--- api_v2/sensitive_api.py
+++ api_v2/sensitive_api.py
@@ -20,10 +20,11 @@ def create_or_modify_sensitive_rule():
     if request.json.get('rule_id') is not None:
         # Modify rule.
         rule_id = request.json.get('rule_id')
+        # Get a available previous rule by its id.
         previous_rule = BaseModel.fetch_by_id('sensitive_message_rule', rule_id)
         if previous_rule is None:
             return response({'err_code': -3, 'err_info': 'Can not find this rule by its id.'})
-        # Get a available previous rule by its id.
+        # Modify this rule.
         value_as_dict = request.json
         previous_rule.chatroom_name_list = value_as_dict["chatroom_name_list"]
         previous_rule.sensitive_word_list = value_as_dict["sensitive_word_list"]
@@ -37,7 +38,7 @@ def create_or_modify_sensitive_rule():
             'owner_list': modified_rule.owner_list,
             'is_work': modified_rule.is_work
         }
-        return response(_10_to_true_false(modified_rule_result))
+        return response({'err_code': 0, 'content': _10_to_true_false(modified_rule_result)})
     else:
         # Create rule.
         new_rule = CM('sensitive_message_rule')
@@ -55,17 +56,61 @@ def create_or_modify_sensitive_rule():
             'owner_list': created_rule.owner_list,
             'is_work': created_rule.is_work
         }
-        return response(_10_to_true_false(created_rule_result))
+        return response({'err_code': 0, 'content': _10_to_true_false(created_rule_result)})
 
 
 @main_api_v2.route('/sensitive_rule_delete', methods=['POST'])
+@para_check("token", "rule_id", )
 def sensitive_rule_delete():
-    pass
+    rule_id = request.json.get('rule_id')
+    this_rule = BaseModel.fetch_by_id('sensitive_message_rule', rule_id)
+    if this_rule is None:
+        return response({'err_code': -3, 'err_info': 'rule_id wrong.'})
+    this_rule.is_work = 0
+    this_rule.save()
+    this_rule_result = {
+        'rule_id': this_rule.sensitive_message_rule_id,
+        'chatroom_name_list': this_rule.chatroom_name_list,
+        'sensitive_word_list': this_rule.sensitive_word_list,
+        'owner_list': this_rule.owner_list,
+        'is_work': this_rule.is_work
+    }
+    return response({'err_code': 0, 'content': _10_to_true_false(this_rule_result)})
 
 
 @main_api_v2.route('/sensitive_rule_list', methods=['POST'])
+@para_check("token", )
 def sensitive_rule_list():
-    pass
+    # Check owner or return.
+    status, user_info = UserLogin.verify_token(request.json.get('token'))
+    try:
+        owner = user_info.username
+    except AttributeError:
+        return response({'err_code': -2, 'content': 'User token error.'})
+
+    all_rule_list = BaseModel.fetch_all('sensitive_message_rule', '*', BaseModel.where_dict({'is_work': 1}))
+    this_owner_rule_list = []
+    content = []
+
+    for rule in all_rule_list:
+        if owner in rule.owner_list:
+            this_owner_rule_list.append(rule)
+
+    for rule in this_owner_rule_list:
+        rule_id = rule.sensitive_message_rule_id
+        # Create chatroom_list
+        chatroom_list = []
+        for chatroomname in rule.chatroom_name_list:
+            this_chatroom = BaseModel.fetch_one('a_chatroom', '*', BaseModel.where_dict({'chatroomname': chatroomname}))
+            avatar_url = this_chatroom.avatar_url
+            nickname = this_chatroom.nickname_real
+            chatroom_list.append(
+                {'avatar_url': avatar_url, 'chatroomname': chatroomname, 'chatroom_nickname': nickname})
+
+        sensitive_word_list = rule.sensitive_word_list
+        content.append({'rule_id': rule_id, 'chatroom_list': chatroom_list, 'sensitive_word_list': sensitive_word_list})
+
+    return response({'err_code': 0, 'content': content})
 
 
 @main_api_v2.route('/sensitive_message_log', methods=['POST'])
