28d9c587255a152795cca29aabe504fa0a9a704d
Quentin Wang <wangkx0105@outlook.com>
2018-04-27 14:59:58 +0800

    add new api of zone

--- api_v2/group_zone.py
+++ api_v2/group_zone.py
@@ -19,6 +19,15 @@ MSG_TYPE_SHARE = 49
 MSG_TYPE_SYS = 10000
 MSG_TYPE_ENTERCHATROOM = 570425393
 
+MSG_TYPE_DICK = {
+    1: ['.txt', '.pdf', '.doc', '.xls'],
+    2: [],
+    3: ['.mp4', 'mov'],
+    4: ['.png', '.jpg', '.jpeg']
+
+}
+
+
 
 
 @main_api_v2.route("/group_zone_lists", methods=['POST'])
@@ -40,8 +49,8 @@ def get_group_zone_list():
     try:
         for client_qun in client_quns:
             chatroom_info = BaseModel.fetch_one("a_chatroom", "*",
-                                                where_clause=BaseModel.where_dict({"chatroomname": client_qun.get('chatroomname')}))
-
+                                                where_clause=BaseModel.where_dict(
+                                                    {"chatroomname": client_qun.get('chatroomname')}))
 
             # client_qun_to_dict = client_qun.to_json_full()
             client_qun.update(chatroom_info.to_json_full())
@@ -61,18 +70,76 @@ def get_group_zone_sources():
     if status != SUCCESS:
         return make_response(status)
 
+    client_id = user_info.client_id
+    try:
+        client_quns = BaseModel.fetch_all("client_qun_r", "*",
+                                          where_clause=BaseModel.where_dict({"client_id": client_id}))
+        client_quns = [client_qun.to_json_full() for client_qun in client_quns]
+    except:
+        return make_response(ERR_WRONG_ITEM)
 
-    talker = request.json.get('talker')
+    # 群id--talker
+    talker = request.json.get('chatroomname')
     keyword = request.json.get('keyword')
     source_type = request.json.get('source_type')
     page = request.json.get('page')
     pagesize = request.json.get('pagesize')
+    order_type = request.json.get('order_type')
 
+    # 获取所有的群空间
+    # if not talker:
+    #     messages = []
+    #     for client_qun in client_quns:
+    #         messages.extend(BaseModel.fetch_all('a_message', '*',
+    #                                             where_clause=BaseModel.where_dict(
+    #                                                {"talker": client_qun.get('chatroomname')}),
+    #                                             page=page, pagesize=pagesize,
+    #                                             order_by=BaseModel.order_by({"create_time": order_type})
+    #                                             )
+    #
+    #                         )
+    if talker:
+        messages = BaseModel.fetch_all('a_message', '*',
+                                       where_clause=BaseModel.where_dict(
+                                            {"talker": talker}),
+                                       page=page, pagesize=pagesize,
+                                       order_by=BaseModel.order_by({"create_time": order_type})
+                                       )
 
-    messages = BaseModel.fetch_all('a_message', '*',
-                                   where_clause=None,
-                                   page = page, pagesize = pagesize,
-                                   orderBy = order, group = group_by)
+if __name__ == "__main__":
 
+    BaseModel.extract_from_json()
+    # messages = BaseModel.fetch_all("a_message", "*",
+    #                                where_clause=BaseModel.where_dict(
+    #                                    {"talker": '10973997003@chatroom',
+    #                                     'create_time': 1524804656000}),
+    #                                pagesize=10, page=1,
+    #                                order_by=BaseModel.order_by({"create_time": "desc"})
+    #                                )
 
-
+    messages = BaseModel.fetch_all("a_message", "*",
+                                   where_clause=BaseModel.or_(
+                                       ['=', 'talker', '5663579223@chatroom'],
+                                       ['=', 'talker', '10973997003@chatroom']
+                                   ),
+                                   pagesize=10, page=1,
+                                   order_by=BaseModel.order_by({"create_time": "desc"})
+                                   )
+    # messages = BaseModel.fetch_all("a_message", "*")
+    print [message.to_json_full() for message in messages].__len__()
+    print messages.__len__()
+    #
+    # # messages = BaseModel.fetch_all('a_message', '*',
+    # #                                where_clause=BaseModel.where_dict({"talker": 'wxid_zy8gemkhx2r222'}),
+    # #                                pagesize=1, page=1)
+    #
+    # print hasattr(messages[0], 'update')
+    # client_quns = BaseModel.fetch_all("client_qun_r", "*",
+    #                                   where_clause=BaseModel.where_dict({"client_id": 2}))
+    #
+    # chatroom_info = BaseModel.fetch_one("a_chatroom", "*",
+    #                                     where_clause=BaseModel.where_dict({"chatroomname": "8835992041@chatroom"}))
+    #
+    #
+    # print client_quns
+    pass
