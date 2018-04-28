cedf55719a9263004ed212e461c20d20a3c45ab7
zhaochang <zhaochang@doodod.com>
2018-04-28 18:31:59 +0800

    FIX

--- api_v2/script.py
+++ api_v2/script.py
@@ -25,5 +25,8 @@ def script():
     # user_info = BaseModel.fetch_one(UserInfo, "*", where_clause = BaseModel.where_dict({"client_id": 15}))
     # user_info.username = u"ada390859"
     # user_info.save()
-    update_coin_all()
+    # update_coin_all()
+    ubr = BaseModel.fetch_one("client_bot_r", "5ae446d7f5d7e26c380d2324")
+    ubr.bot_username = "wxid_lkruzsl7w2r822"
+    ubr.save()
     return make_response(SUCCESS)
