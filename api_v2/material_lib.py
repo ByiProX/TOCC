5e8065acf27b7c873d4bd407ec77e61ac209ba37
Quentin Wang <wangkx0105@outlook.com>
2018-05-03 14:32:47 +0800

    add material_lib

---
+++ api_v2/material_lib.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from flask import request
+from configs.config import main_api_v2, SUCCESS, ERR_WRONG_ITEM
+from core_v2.user_core import UserLogin
+from models_v2.base_model import BaseModel
+from utils.u_model_json_str import verify_json
+from utils.u_response import make_response
+
+
+@main_api_v2.route("/material_lib_list", methods=['POST'])
+def read_material_lib_list():
+    verify_json()
+    status, user_info = UserLogin.verify_token(request.json.get('token'))
+    if status != SUCCESS:
+        return make_response(status)
+
+    client_id = user_info.client_id
+    page = request.json.get('page')
+    pagesize = request.json.get('pagesize')
+    order_type = request.json.get('order_type', 'desc')
+
+    try:
+        materials = BaseModel.fetch_all("material_lib", "*",
+                                        where_clause=BaseModel.where_dict(
+                                            {"client_id": client_id}),
+                                        page=page, pagesize=pagesize,
+                                        order_by=BaseModel.order_by({"create_time": order_type}
+                                                                    ))
+    except:
+        return make_response(ERR_WRONG_ITEM)
