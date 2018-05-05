897de400bd0125ff9eeb0c847c8c00e9c89b57a1
zhaochang <zhaochang@doodod.com>
2018-05-05 18:43:08 +0800

    FEAT

--- api_v2/android_api.py
+++ api_v2/android_api.py
@@ -46,12 +46,38 @@ def android_add_friend():
     return make_response(SUCCESS)
 
 
+@main_api_v2.route('/android/add_material', methods=['POST'])
+def android_add_friend():
+    verify_json()
+    # user_nickname = request.json.get('user_nickname')
+    # user_username = request.json.get('user_username')
+    # bot_username = request.json.get('bot_username')
+    # bot = BaseModel.fetch_one(BotInfo, '*', where_clause = BaseModel.where_dict({"username": bot_username}))
+    #
+    # logger.info(u"发现加bot好友用户. username: %s." % user_username)
+    # status, user_info = _bind_bot_success(user_nickname, user_username, bot)
+    # if status == SUCCESS:
+    #     if user_info.app == "yaca":
+    #         we_conn = wechat_conn_dict.get(user_info.app)
+    #         if we_conn is None:
+    #             logger.info(
+    #                 u"没有找到对应的 app: %s. wechat_conn_dict.keys: %s." % (user_info.app, json.dumps(wechat_conn_dict.keys())))
+    #         we_conn.send_txt_to_follower(
+    #             "您好，欢迎使用数字货币友问币答！请将我拉入您要管理的区块链社群，拉入成功后即可为您的群提供实时查询币价，涨幅榜，币种成交榜，交易所榜，最新动态，行业百科等服务。步骤如下：\n拉我入群➡确认拉群成功➡ "
+    #             "机器人在群发自我介绍帮助群友了解规则➡群友按照命令发关键字➡机器人回复➡完毕",
+    #             user_info.open_id)
+    #         # else:
+    #         #     EmailAlert.send_ue_alert(u"有用户尝试绑定机器人，但未绑定成功.疑似网络通信问题. "
+    #         #                              u"user_username: %s." % user_username)
+
+    return make_response(SUCCESS)
+
+
 @main_api_v2.route("/android/new_message", methods=['POST'])
 def android_new_message():
     verify_json()
     a_message = CM(Message).from_json(request.json)
     a_message.set_id(request.json.get('a_message_id'))
-    a_message.create_time = int(a_message.create_time / 1000)
 
     global gm_rule_dict
     global gm_default_rule_dict
