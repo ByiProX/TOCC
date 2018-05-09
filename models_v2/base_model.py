002655e63ce838797d0b2398c459b07b1f500fda
zhaochang <zhaochang@doodod.com>
2018-05-09 21:07:25 +0800

    FIX

--- models_v2/base_model.py
+++ models_v2/base_model.py
@@ -349,8 +349,7 @@ class BaseModel(object):
                 eof = True
                 return item_list
             response = requests.get(url = url, params = query_clause)
-            if where_clause:
-                print response.request.url
+            # print response.request.url
             page += 1
             query_clause["page"] = page
             # if group:
