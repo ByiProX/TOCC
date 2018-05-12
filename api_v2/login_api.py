1c8be31f7c637092d53028a8d828a13443ada1b3
Quentin Wang <wangkx0105@outlook.com>
2018-05-12 18:28:22 +0800

    fix

--- api_v2/login_api.py
+++ api_v2/login_api.py
@@ -72,20 +72,20 @@ def app_get_user_basic_info():
 
     status, res = cal_user_basic_page_info(user_info)
 
-    # client_info = BaseModel.fetch_one("a_contact", "*",
-    #                                   where_clause=BaseModel.where_dict(
-    #                                       {"username": user_info.username}
-    #                                   ))
-    # client_info = client_info.to_json_full()
-    # client_info["app"] = user_info.app
+    client_info = BaseModel.fetch_one("a_contact", "*",
+                                      where_clause=BaseModel.where_dict(
+                                          {"username": user_info.username}
+                                      ))
+    client_info = client_info.to_json_full()
+    client_info["app"] = user_info.app
 
     if status == SUCCESS:
         return make_response(status, bot_info=res['bot_info'], user_func=res['user_func'],
-                             total_info=res['total_info'], client_info="client_info")
+                             total_info=res['total_info'], client_info=client_info)
     # 目前INFO均返回为SUCCESS
     elif status == INFO_NO_USED_BOT:
         return make_response(SUCCESS, bot_info=res['bot_info'], user_func=res['user_func'],
-                             total_info=res['total_info'], client_info="client_info")
+                             total_info=res['total_info'], client_info=client_info)
     else:
         return make_response(status)
 
