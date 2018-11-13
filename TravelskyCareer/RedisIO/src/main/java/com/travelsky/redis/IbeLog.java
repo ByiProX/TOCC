74fee349a5b018aa0b6fcc5f01322299c156b52f
Quentin Wang <wangkx0105@outlook.com>
2018-11-13 19:07:37 +0800

    cmp ibelog class

--- TravelskyCareer/RedisIO/src/main/java/com/travelsky/redis/IbeLog.java
+++ TravelskyCareer/RedisIO/src/main/java/com/travelsky/redis/IbeLog.java
@@ -55,10 +55,69 @@ public class IbeLog implements Metric{
 
 class IbeLogValue implements MetricValue{
 
-//    value: 时间|主机名|时间周期|TPS|最大响应时间|最小响应时间|平均响应时间
-//    0.6.151.21|ibelog|AVH   20180517-23:46:01|pe6850d-vmwc5-ibe|60|63.02|1911|130|404
-
     private String time;
     private String hostname;
+    private int period;
+    private double tps;
+    private int maxResponseTime;
+    private int minResponseTime;
+    private int aveResponseTime;
 
+    public String getTime() {
+        return time;
+    }
+
+    @Override
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    @Override
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public int getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(int period) {
+        this.period = period;
+    }
+
+    public double getTps() {
+        return tps;
+    }
+
+    public void setTps(double tps) {
+        this.tps = tps;
+    }
+
+    public int getMaxResponseTime() {
+        return maxResponseTime;
+    }
+
+    public void setMaxResponseTime(int maxResponseTime) {
+        this.maxResponseTime = maxResponseTime;
+    }
+
+    public int getMinResponseTime() {
+        return minResponseTime;
+    }
+
+    public void setMinResponseTime(int minResponseTime) {
+        this.minResponseTime = minResponseTime;
+    }
+
+    public int getAveResponseTime() {
+        return aveResponseTime;
+    }
+
+    public void setAveResponseTime(int aveResponseTime) {
+        this.aveResponseTime = aveResponseTime;
+    }
 }
